92ecf6b828d6d3673802c79481fe692c19e32e40
idouaddi-ayoub <113615894+idouaddi-ayoub@users.noreply.github.com>
2024-03-24 22:33:41 +0000

    First triangle

--- Core/Source/Core/Core.cpp
+++ Core/Source/Core/Core.cpp
@@ -2,46 +2,7 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
 #include <iostream>
-
-namespace Core 
-{
-
-	void testFunc()
-	{
-		int x{ getUserValue() }, y{ getUserValue() };
-		std::cout << Calc::multiple(x, y);
-		std::cout << Calc::sum(x, y);
-	}
-
-	int getUserValue()
-	{
-		std::cout << "\nEnter int\n";
-
-		int input{};
-		std::cin >> input;
-
-		return input;
-	}
-
-}
-
-namespace Calc {
-
-	int multiple(int x, int y)
-	{
-		std::cout << "\nMutliple: ";
-		return x * y;
-	}
-
-	int sum(int x, int y)
-	{
-		std::cout << "\nSummation: ";
-		return x + y;
-	}
-
-}
 
 namespace openGL {
 
@@ -50,6 +11,20 @@ namespace openGL {
 
     unsigned int SCR_WIDTH{ 800 };
     unsigned int SCR_HEIGHT{ 600 };
+
+    const char *vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+    
+    const char *fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\n\0";
 
     int opengl(void)
     {
@@ -63,7 +38,7 @@ namespace openGL {
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-        /* Create a windowed mode window and its OpenGL context */
+        /* Create a windowed mode window and its OpenGL's context */
         GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Lbzboz", NULL, NULL);
         if (window == NULL)
         {
@@ -76,13 +51,90 @@ namespace openGL {
         glfwMakeContextCurrent(window);
         glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-        /*glad*/
-      if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        // glad: load all OpenGL function pointers
+        // ---------------------------------------
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
         {
-            std::cout << "failed to initialize gald" << std::endl;
+            std::cout << "failed to initialize glad" << std::endl;
 
             return -1;
         }
+
+        // build and compile our shader program
+        // ------------------------------------
+        // vertex shader
+        unsigned int vertexShader;
+        vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+        glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+        glCompileShader(vertexShader);
+
+        int  success;
+        char infoLog[512];
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+        if (!success)
+        {
+            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+
+        //fragment shader
+        unsigned int fragmentShader;
+        fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+        glCompileShader(fragmentShader);
+
+        //check for shader compile errors
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+
+        //link shaders
+        unsigned int shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+
+        //check for linking errors
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        }
+
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+        //set up vertex data
+        float vertices[] = {
+            -0.5f, -0.5f, 0.0f, // left
+             0.5f, -0.5f, 0.0f, // right
+             0.0f,  0.5f, 0.0f, // top
+        };
+
+        unsigned int VBO{}, VAO{};
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+
+        // 1. Biding array
+        glBindVertexArray(VAO);
+        
+        // 2. Copy the vertices array in a buffar 
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        
+        // 3. Setting vertex attributes pointers
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        // 4. Drawing the object
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
@@ -94,6 +146,11 @@ namespace openGL {
             glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
 
+            // drawing triangle
+            glUseProgram(shaderProgram);
+            glBindVertexArray(VAO);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
 
@@ -101,11 +158,17 @@ namespace openGL {
             glfwPollEvents();
         }
 
-        glfwTerminate();
+        // de-allocate all resources once they've outlived their purpose
+        glDeleteVertexArrays(1, &VAO);
+        glDeleteBuffers(1, &VBO);
+        glDeleteProgram(shaderProgram);
 
+        //terminate allocated GLFW resources
+        glfwTerminate();
         return 0;
     }
 
+    // process all input
     void processInput(GLFWwindow* window)
     {
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -114,7 +177,7 @@ namespace openGL {
 
     // glfw: whenever the window size changed (by OS or user resize) this callback function executes
     // ---------------------------------------------------------------------------------------------
-    void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+    void framebuffer_size_callback(GLFWwindow*, int width, int height)
     {
         // make sure the viewport matches the new window dimensions; note that width and 
         // height will be significantly larger than specified on retina displays.
